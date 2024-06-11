7a6756b61c830f67f0c84bc3956116fb8926e282
Yoo <ejyoo921@gmail.com>
2024-06-11 09:46:41 -0700

    make co2snow inlet geom using EB-lathe: use the uploaded input file. Runing on Kestrel for EB-C9 type flow.

--- Exec/RegTests/EB-CA/prob.cpp
+++ Exec/RegTests/EB-CA/prob.cpp
@@ -1,4 +1,6 @@
 #include "prob.H"
+// EY: for co2snow inlet
+#include <AMReX_EB2_IF_Spline.H>
 
 void
 pc_prob_close()
@@ -130,4 +132,109 @@ PeleC::problem_post_init()
 void
 PeleC::problem_post_restart()
 {
+}
+
+// EY: Parm_parse
+void
+parse_params(ProbParmDevice* prob_parm_device, const amrex::Real* problo, const amrex::Real* probhi)
+{
+  // Parse params
+  amrex::ParmParse pp("prob");
+  amrex::ParmParse ppe("eb2");
+  std::string geom_type;
+  ppe.get("geom_type", geom_type);
+
+  if (geom_type == "co2snow-inlet") {
+    amrex::ParmParse ppn("nozzle");
+    amrex::Vector<amrex::Real> yvals;
+    amrex::Vector<amrex::Real> xvals;
+    ppn.getarr("xvals",xvals);
+    ppn.getarr("yvals",yvals);
+    amrex::Real offset;
+    amrex::Real factor;
+    ppn.get("offset",offset);
+    ppn.get("factor",factor);
+    const int nvals = xvals.size();
+    for (int i =0; i < nvals; ++i) {
+      yvals[i] = offset + factor * yvals[i];
+    }
+    int i;
+    for (i =0; i < nvals; ++i) {
+      if (problo[0] < xvals[i]) {
+        break;
+      }
+    }
+    amrex::Real r_in = yvals[i];
+    for (i = i; i < nvals-1; ++i) {
+      if (yvals[i+1] > yvals[i]) {
+        break;
+      }
+    }
+    amrex::Real r_t = yvals[i];
+    for (i = i; i < nvals; ++i) {
+      if (probhi[0] < xvals[i]) {
+        break;
+      }
+    }
+    amrex::Real r_out = yvals[i-1];
+
+    bool do_lathe;
+    ppn.get("do_lathe", do_lathe);
+    prob_parm_device->aoveras_in = (r_in/r_t);
+    prob_parm_device->aoveras_out = (r_out/r_t);
+    if (do_lathe) {
+      prob_parm_device->aoveras_in *= prob_parm_device->aoveras_in;
+      prob_parm_device->aoveras_out *= prob_parm_device->aoveras_out;
+    }
+    prob_parm_device->geom_type = 2;
+
+  }
+  else {
+    amrex::Abort("Invalid geom_type specified");
+  }
+}
+
+void
+EBco2snowInlet::build( // We don't do spline here; only lathe
+  const amrex::Geometry& geom, const int max_coarsening_level)
+{
+
+  amrex::EB2::SplineIF nozzle_upper_surface;
+  std::vector<amrex::RealVect> lnpts;
+
+  amrex::ParmParse pp("nozzle");
+  amrex::Real offset = 1.0;
+  amrex::Real factor = 1.0;
+  bool do_spline = false;
+  bool do_lathe = true;
+  pp.query("offset", offset);
+  pp.query("factor", factor);
+  pp.query("do_spline", do_spline);
+  pp.query("do_lathe", do_lathe);
+
+  amrex::Vector<amrex::Real> xvals;
+  amrex::Vector<amrex::Real> yvals;
+  pp.getarr("xvals",xvals);
+  pp.getarr("yvals",yvals);
+  int nvals = xvals.size();
+  AMREX_ALWAYS_ASSERT(yvals.size() == nvals);
+  for (int i = 0; i < nvals; ++i) {
+    yvals[i] = offset + factor * yvals[i];
+  }
+
+  for (int ii =0;ii<nvals; ++ii) {
+    lnpts.push_back(amrex::RealVect(AMREX_D_DECL(xvals[ii], yvals[ii], 0.0)));
+  }
+  nozzle_upper_surface.addLineElement(lnpts);
+
+  // lathe only supported in 3D
+  AMREX_ALWAYS_ASSERT(AMREX_SPACEDIM==3);
+  // revolving around x-axis
+  auto nozzle_rotate = amrex::EB2::rotate(nozzle_upper_surface, -0.5*constants::PI(), 2);
+  auto nozzle_lathe = amrex::EB2::lathe(nozzle_rotate);
+  auto nozzle = amrex::EB2::rotate(nozzle_lathe, -0.5*constants::PI(), 1);
+
+  auto gshop = amrex::EB2::makeShop(nozzle);
+  amrex::EB2::Build(gshop, geom, max_coarsening_level, max_coarsening_level, 4, false);
+
 }
